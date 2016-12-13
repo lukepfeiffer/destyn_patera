ee295d1d18a79be7f745b0bd9cd6db0fdc4dcad9
Hashrocket Workstation <dev@hashrocket.com>
2016-12-12 21:23:19 -0500

    WIP javascript videos only works once

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
+
   $('.video-container').on('click', '.thumb-nail', function(){
     var thumbNail = $(this)
     var videoId = thumbNail.data('id')
     var videoIframe = $('#' + videoId).children('iframe')
-    debugger
     $('#' + videoId).show();
   });
+
+  $(document).on('click', '#main', function(event) {
+    if(!$(event.target).is('.thumb-nail, img, iframe')){
+      if($('iframe').is(":visible")) {
+        $('iframe').hide();
+      }
+    };
+  });
+
 });
