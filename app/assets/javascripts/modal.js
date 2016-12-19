05b3bb49dadc5de875b302bc1b971b66068f9d37
Hashrocket Workstation <dev@hashrocket.com>
2016-12-19 11:54:41 -0500

    Center modal

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
+
   $('#photos').on('click', '.modal-trigger', function(){
     var category = $(this);
+    jQuery.fn.center = function () {
+      this.css("position","absolute");
+      this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + $(window).scrollTop()) + "px");
+      this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+      return this;
+    }
 
     $.ajax({type: 'get',
       url: category.data('url'),
       success: function(response){
         $('#main').append(response);
+        $('.modal-container').center();
       }
     });
   });
