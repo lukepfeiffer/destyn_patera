48eed7d26d2db7b222c00e489620faeb0ae07d94
Hashrocket Workstation <dev@hashrocket.com>
2016-12-23 17:42:46 -0500

    Create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,8 +8,22 @@ class CategoriesController < ApplicationController
   def photos
   end
 
+  def create
+    if category.save
+      redirect_to photos_path
+    else
+      redirect_to new_categories_path
+    end
+  end
+
   def show
     render partial: 'modal', locals: {category: category}
+  end
+
+  def category_params
+    params.require(:category).permit(
+      :title
+    )
   end
 
 end
