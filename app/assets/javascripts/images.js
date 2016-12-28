d2d794bddc9d17d14c49c3f78c9ba44f5924da8c
Hashrocket Workstation <dev@hashrocket.com>
2016-12-28 12:15:19 -0500

    Images slider working

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#images').on('submit', '#new', function(event){
     event.preventDefault();
-    var form = $(this)
+    var form = $(this);
 
     $.ajax({type: "post",
       url: form.attr("action"),
@@ -13,27 +13,29 @@ $(document).ready(function() {
   });
 
   $('#main').on('click', '.next', function(){
-    var allImages = $('.photos').children('img')
-    var currentImageIndex = allImages.index('.photos img:visible')
-    var currentImage = allImages.eq(currentImageIndex)
-    var nextImageIndex = currentImageIndex + 1
-    var nextImage = allImages.eq(nextImageIndex)
-    if(!nextImage){
-      nextImage = allImages.eq(0)
+    var allImages = $('.photos img');
+    var currentImage = $('.photos img:visible').eq(0);
+    var currentImageIndex = allImages.index(currentImage);
+    var nextImageIndex = currentImageIndex + 1;
+    if (allImages.eq(nextImageIndex).length == 0) {
+      nextImageIndex = 0;
     }
-    currentImage.hide()
-    nextImage.show()
+    var nextImage = allImages.eq(nextImageIndex);
+    allImages.hide();
+    nextImage.show();
   });
 
   $('#main').on('click', '.previous', function(){
-    var allImages = $('.photos').children('img')
-    if(allImages.indexOf('img:visible') == 0){
-      allImages.last().show()
-      allImages.first('img:visible').hide()
-    } else{
-      allImages.first('img:visible').prev().show()
-      allImages.first('img:visible').hide()
+    var allImages = $('.photos img');
+    var currentImage = $('.photos img:visible').eq(0);
+    var currentImageIndex = allImages.index(currentImage);
+    var prevImageIndex = currentImageIndex - 1;
+    if (allImages.eq(prevImageIndex).length == 0) {
+      prevImageIndex = allImages.length - 1;
     }
+    var prevImage = allImages.eq(prevImageIndex);
+    allImages.hide();
+    prevImage.show();
   });
 
 });
--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('#main').on('click', '.overlay', function(event) {
     if(!$(event.target).is('.modal-container')){
       if($('.modal-container').is(":visible")) {
-        $('.modal-container').hide();
+        $('.modal-container').remove();
       }
     };
   });
@@ -12,18 +12,11 @@ $(document).ready(function() {
 
   $('#photos').on('click', '.modal-trigger', function(){
     var category = $(this);
-    jQuery.fn.center = function () {
-      this.css("position","fixed");
-      this.css("top", Math.max(0, (($(window).height())/ 58) + $(window).scrollTop()) + "px");
-      this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 6) + $(window).scrollLeft()) + "px");
-      return this;
-    }
 
     $.ajax({type: 'get',
       url: category.data('url'),
       success: function(response){
         $('#main').append(response);
-        $('.modal-container').center();
       }
     });
   });
@@ -33,9 +26,7 @@ $(document).ready(function() {
   $('.row').on('click', '.thumb-nail', function(){
     var video = $(this);
     jQuery.fn.center = function () {
-      this.css("position","absolute");
-      this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 7) + $(window).scrollTop()) + "px");
-      this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 6) + $(window).scrollLeft()) + "px");
+      this.css("margin-top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 7) + $(window).scrollTop()) + "px");
       return this;
     }
 
