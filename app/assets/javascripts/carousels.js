2c249048663fc65e1ab00bde82eccaf10a5cb60f
Hashrocket Workstation <dev@hashrocket.com>
2017-01-12 09:10:49 -0500

    Mobile scroller

--- app/assets/javascripts/carousels.js
+++ app/assets/javascripts/carousels.js
@@ -1,33 +1,19 @@
 $(document).ready(function() {
+  // normal view
   $('.scroller').slick({
     infinite: true,
     speed: 300,
     slidesToShow: 4,
+    slidesToScroll: 1
+  });
+
+  // mobile view
+  $('.mobile-scroller').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
     slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+    nextArrow: '<div class="class-to-style"><span class="fa fa-arrow-circle-right"></span><span class="sr-only">Next</span></div>',
+    prevArrow: '<div class="class-to-style"><span class="fa fa-arrow-circle-left"></span><span class="sr-only">Next</span></div>'
   });
 });
