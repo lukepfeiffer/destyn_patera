d662b349f0667b721f63d70499c11c7fd1062474
Hashrocket Workstation <dev@hashrocket.com>
2017-01-13 09:27:58 -0500

    Add autoscroll to mobile logos

--- app/assets/javascripts/carousels.js
+++ app/assets/javascripts/carousels.js
@@ -11,8 +11,10 @@ $(document).ready(function() {
   $('.mobile-scroller').slick({
     infinite: true,
     speed: 300,
-    slidesToShow: 2,
+    slidesToShow: 3,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
     nextArrow: '<div class="class-to-style"><span class="fa fa-arrow-circle-right"></span><span class="sr-only">Next</span></div>',
     prevArrow: '<div class="class-to-style"><span class="fa fa-arrow-circle-left"></span><span class="sr-only">Next</span></div>'
   });
