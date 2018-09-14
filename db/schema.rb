13a11db00368ca121ba5cccbed90493789d320b0
lukepfeiffer <lukerpfeiffer@gmail.com>
2018-09-14 17:10:50 -0400

    Initial setup on new laptop

--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 20170320032918) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "dropbox_url"
   end
 
